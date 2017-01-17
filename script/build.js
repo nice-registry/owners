d2847004bac937da5a7a285cd079661db04ebc19
Zeke Sikelianos <zeke@sikelianos.com>
2017-01-17 12:53:15 -0800

    started get 429 errors at 10 per second

--- script/build.js
+++ script/build.js
@@ -2,7 +2,7 @@ const registry = require('package-stream')()
 const npmUser = require('npm-user')
 const cleanDeep = require('clean-deep')
 const RateLimiter = require('limiter').RateLimiter
-const limiter = new RateLimiter(10, 'second')
+const limiter = new RateLimiter(5, 'second')
 const owners = {}
 
 registry
